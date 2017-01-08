00fdf5241e3d7d5f661b296af02ab8e1b99c0b57
Asetr <asetre.paul@gmail.com>
2017-01-07 19:48:13 -0800

    added before and after inside describe blocks

--- test/api-tests.js
+++ test/api-tests.js
@@ -11,6 +11,86 @@ const TEST_DATABASE_URL = 'mongodb://localhost/blog-app-tests';
 
 chai.use(chai-http);
 
+function generateBlogPosts {
+    return [
+		{
+		    author: {firstName: 'John', lastName: 'Smith'},
+
+		    title: 'Hello World!',
+		    content: 'This is my first post',
+		    created: Date.now
+		},
+		
+		{
+		    author: {firstName: 'Smith', lastName: 'John'},
+
+		    title: 'Goodbye World!',
+		    content: 'This is my second post',
+		    created: Date.now
+		},
+		
+		{
+		    author: {firstName: 'Fizz', lastName: 'Buzz'},
+
+		    title: 'Bizz Bang!!',
+		    content: 'Did you expect this?',
+		    created: Date.now
+		},
+
+		{
+		    author: {firstName: 'Scooby', lastName: 'Doo'},
+
+		    title: 'Woof!',
+		    content: 'Wooof woof woof woof!',
+		    created: Date.now
+		},
+
+    ]	    
+}
+
+function seedBlogPosts() {
+
+    var seedData = generateBlogPosts();
+
+    return BlogPost.insertMany(seedData);
+}
+
+function deleteDatabase() {
+    console.log('Deleting database');
+    return mongoose.connection.dropDatabase();
+}
+
+describe('Blog Posts API resource', function() {
+    
+    before(function() {
+	return runServer(TEST_DATABASE_URL);
+
+
+    });
+
+    beforeEach(function() {
+        return seedBlogPosts();    
+    });
+
+    afterEach(function() {
+	return deleteDatabase();
+    });
+
+    after(function() {
+	return closeServer();
+    });
+
+
+});
+
+
+
+
+
+
+
+
+
 
 
 
