180907954b47fd1f7d27c689b355649ae0f011b5
Asetr <asetre.paul@gmail.com>
2017-01-08 16:49:06 -0800

    added post test

--- test/api-tests.js
+++ test/api-tests.js
@@ -65,23 +65,63 @@ describe('Blog Posts API resource', function() {
         });
 
         it('should return the right fields', function(){
-            let resPosts;
             return chai.request(app)
                 .get('/posts')
                 .then(function(res) {
                     res.should.have.status(200);
                     res.should.be.json;
-                    res.body.posts.should.be.a('array');
-
-                })  
-            
-            
+                    res.body.should.be.a('array');
+                    res.body.forEach(function(posts){
+                        posts.should.be.a('object');
+                        posts.title.should.be.a('string');
+                        posts.content.should.be.a('string');
+                        posts.author.should.be.a('string');
+                    });
+                });
         });
     });
 
-    //    describe('POST /posts, to create a post inside the database', function() {
+    describe('POST endpoint', function() {
+        it ('should create a post and save it to the database', function() {
+            const newPost = {
+                author: {firstName: 'John', lastName: 'Smith'},
+                title: 'test',
+                content: 'this is a test'
+            };
 
-    //    });
+            return chai.request(app)
+                .post('/posts')
+                .send(newPost)
+                .then(function(res){
+                    res.should.have.status(201);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.include.keys('id', 'title', 'content', 'author', 'created');
+                    res.body.title.should.equal(newPost.title);
+                    res.body.content.should.equal(newPost.content);
+                    res.body.author.should.equal(`${newPost.author.firstName} ${newPost.author.lastName}`);
+                    res.body.id.should.not.be.null;
+
+                    return BlogPost.findById(res.body.id).exec();
+                })
+                .then(function(post) {
+                    post.title.should.equal(newPost.title);
+                    post.content.should.equal(newPost.content);
+                    post.author.firstName.should.equal(newPost.author.firstName);
+                    post.author.lastName.should.equal(newPost.author.lastName);
+                }); 
+        });
+
+    });
+
+    describe('PUT endpoint' function() {
+
+
+
+
+
+
+    });
 });
 
 
